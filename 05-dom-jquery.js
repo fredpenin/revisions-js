ee1e22bd16e5182265e91a5db71368261c44a64d
fredpenin <fred.penin@gmail.com>
2018-11-08 10:41:10 +0100

    Maj / end revisions js

--- 05-dom-jquery.js
+++ 05-dom-jquery.js
@@ -2,22 +2,63 @@
 
 // On cible le bouton et on l'affecte a la 
 // variable btn_move
-
+var btn_move = $('#move');
 
 // On cible l'element .square
-
+var square = $('.square');
 
 // Quand on clic sur le bouton...
-// btn_move.addEventListener('click', function(){
-//     //....
+// btn_move.click(function(){ alert('YOP'); });
+btn_move.on('click', do_on_clic);
+// btn_move.on({
+//     'click': do_on_clic,
+//     // 'mouseover' : function(){ alert('Yep'); }
 // });
-var btn_move = 
+function do_on_clic() {
+    if ( square.css('left') == "0px" ) {
+        // square.css('right', '0px');
+        // square.css('left', '');
+        square.css({
+            left: '',
+            right: '0px'
+        });
+    }
+    else {
+        // square.css('left', '0px');
+        // square.css('right', '');
+        square.css({
+            left: '0px',
+            right: ''
+        });
+    }
+}
 
 ////
 
 // Cibler les champs
-
+var champ_1 = $('#champ_2');
+var duplicate = $('#duplication');
 
 // Evenement de clavier sur champ 1
+// champ_1.on('keyup', do_on_keyup);
+// function do_on_keyup(){
+//     duplicate.val( champ_1.val() );
+// }
+// champ_1.on('blur', do_on_blur);
+// function do_on_blur() {
+//     square.html( duplicate.val() );
+// }
 
-    // recup la valeur du champ
+function do_on_keyup(){
+    duplicate.val( champ_1.val() );
+}
+function do_on_blur() {
+    square.html( duplicate.val() );
+    champ_1.val('');
+    duplicate.val('');
+}
+
+champ_1.on({
+    keyup: do_on_keyup,
+    blur: do_on_blur
+});
